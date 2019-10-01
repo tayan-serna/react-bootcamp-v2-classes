751aa7b42f032129214f39fee4c2b186ac5f4269
Adrian Serna Gomez <aserna@YUXMED0029M.local>
2019-10-01 15:35:14 -0500

    class 5

--- src/components/Details/index.js
+++ src/components/Details/index.js
@@ -1,79 +1,80 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 const RickAndMortyAPI = 'https://rickandmortyapi.com/api/character/';
-class Details extends Component {
-  state = {
-    character: {},
-    loading: true
-  };
 
-  componentDidMount() {
-    const { match } = this.props;
-    fetch(`${RickAndMortyAPI}${match.params.id}`)
+const Details = props => {
+  const [character, setCharacter] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [favorite, setFavorite] = useState(false);
+
+  useEffect(() => {
+    fetch(`${RickAndMortyAPI}${props.match.params.id}`)
       .then(res => res.json())
       .then(character => {
-        this.setState({
-          character,
-          loading: false
-        });
+        setCharacter(character);
+        setLoading(false);
       });
+  }, []);
+
+  const handleImageClick = () => {
+    setFavorite(!favorite);
+  };
+
+  if (loading) {
+    return <span>Loading ...</span>;
   }
 
-  render() {
-    const {
-      history: { goBack }
-    } = this.props;
-    const { loading, character } = this.state;
-
-    if (loading) {
-      return <span>Loading ...</span>;
-    }
-    return (
-      <div className="App_detail">
-        <div className="App_detail_image-container">
-          <span
-            role="img"
-            aria-label="back"
-            className="App_detail__back"
-            onClick={() => goBack()}
-          >
-            🔙
-          </span>
-          <img className="App_detail__image" src={character.image} />
+  return (
+    <div className="App_detail">
+      <div className="App_detail_image-container">
+        <span
+          role="img"
+          aria-label="back"
+          className="App_detail__back"
+          onClick={() => props.history.goBack()}
+        >
+          🔙
+        </span>
+        <img
+          onDoubleClick={handleImageClick}
+          className="App_detail__image"
+          src={character.image}
+        />
+        {favorite && (
           <span role="img" aria-label="star" className="App_detail__favorite">
             ⭐
           </span>
+        )}
+      </div>
+      <div className="App_detail__details">
+        <div className="App_detail__item">
+          <strong className="App_detail__label">NAME: </strong>
+          <span className="App_datail_info">
+            {character.name}
+            <span>({character.status})</span>
+          </span>
         </div>
-        <div className="App_detail__details">
-          <div className="App_detail__item">
-            <strong className="App_detail__label">NAME: </strong>
-            <span className="App_datail_info">
-              {character.name}
-              <span>({character.status})</span>
-            </span>
-          </div>
-          <div className="App_detail__item">
-            <strong className="App_detail__label">SPECIE: </strong>
-            <span className="App_datail_info">Human</span>
-          </div>
-          <div className="App_detail__item">
-            <strong className="App_detail__label">TYPE: </strong>
-            <span className="App_datail_info">{character.type || 'N/A'}</span>
-          </div>
-          <div className="App_detail__item">
-            <strong className="App_detail__label">GENDER: </strong>
-            <span className="App_datail_info">{character.gender}</span>
-          </div>
-          <div className="App_detail__item">
-            <strong className="App_detail__label">LOCATION: </strong>
-            <span className="App_datail_info">{character.location.name}</span>
-          </div>
+        <div className="App_detail__item">
+          <strong className="App_detail__label">SPECIE: </strong>
+          <span className="App_datail_info">Human</span>
+        </div>
+        <div className="App_detail__item">
+          <strong className="App_detail__label">TYPE: </strong>
+          <span className="App_datail_info">{character.type || 'N/A'}</span>
+        </div>
+        <div className="App_detail__item">
+          <strong className="App_detail__label">GENDER: </strong>
+          <span className="App_datail_info">{character.gender}</span>
+        </div>
+        <div className="App_detail__item">
+          <strong className="App_detail__label">LOCATION: </strong>
+          <span className="App_datail_info">{character.location.name}</span>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 Details.propTypes = {
   match: PropTypes.shape({
